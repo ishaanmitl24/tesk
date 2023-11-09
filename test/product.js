b0ba60fea40d26039dfd4bfae823b340b78a1d03
Ishaan Mittal <111686017+ishaanmitl24@users.noreply.github.com>
2023-11-09 15:04:49 +0530

    Add files via upload

---
+++ test/product.js
@@ -0,0 +1,140 @@
+import Samsung from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+import image4 from "../assets/image4.jpg";
+import image5 from "../assets/image5.jpg";
+export  const Products = [
+    {
+      id: Math.random().toString(),
+      data: "Box1",
+      image: Samsung,
+      discount: "40",
+      name: "Samsung",
+      price: "20,000",
+      realPrice: 20000,
+      rating: 5,
+      user:77
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box2",
+      image: image2,
+      discount: "30",
+      name: "Laptop",
+      price: "40,000",
+      realPrice: 40000,
+      rating: 4,
+      user:75
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box3",
+      image: image3,
+      discount: "35",
+      name: "MacBook",
+      price: "1,00,000",
+      realPrice: 100000,
+      rating: 4,
+      user:87
+    },
+  
+    {
+      id: Math.random().toString(),
+      data: "Box4",
+      image: image4,
+      discount: "25",
+      name: "Bags",
+      price: "4,000",
+      realPrice: 4000,
+      rating: 5,
+      user:70
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box5",
+      image: image5,
+      discount: "45",
+      name: "Bed",
+      price: "8,000",
+      realPrice: 8000,
+      rating: 5,
+      user:75
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box6",
+      image: Samsung,
+      discount: "40",
+      name: "Samsung",
+      price: "20,000",
+      realPrice: 20000,
+      rating: 5,
+      user:77
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box7",
+      image: image2,
+      discount: "30",
+      name: "Laptop",
+      price: "40,000",
+      realPrice: 40000,
+      rating: 4,
+      user:87
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box8",
+      image: image3,
+      discount: "35",
+      name: "MacBook",
+      price: "1,00,000",
+      realPrice: 100000,
+      rating: 4,
+      user:107
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box9",
+      image: image4,
+      discount: "25",
+      name: "Bags",
+      price: "4,000",
+      realPrice: 4000,
+      rating: 5,
+      user:75
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box10",
+      image: image5,
+      discount: "44",
+      name: "Bed",
+      price: "8,000",
+      realPrice: 8000,
+      rating: 5,
+      user:70
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box11",
+      image: Samsung,
+      discount: "40",
+      name: "Samsung",
+      price: "20,000",
+      realPrice: 20000,
+      rating: 5,
+      user:77
+    },
+    {
+      id: Math.random().toString(),
+      data: "Box12",
+      image: image2,
+      discount: "30",
+      name: "Laptop",
+      price: "40,000",
+      realPrice: 40000,
+      rating:4,
+      user:100
+    },
+  ];
